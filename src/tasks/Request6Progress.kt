04b2a86cf2471cca0b73d4f4d56113a371d938d9
ELukin <ELukin@it-one.ru>
2022-12-01 19:41:12 +0300

    added solutions

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -7,5 +7,19 @@ suspend fun loadContributorsProgress(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    val allUsers = mutableListOf<User>()
+    repos.map { repo ->
+        val repoUsers = service.getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+                .aggregate()
+        allUsers.addAll(repoUsers)
+        updateResults.invoke(allUsers.aggregate(), false)
+    }
+    updateResults.invoke(allUsers.aggregate(), true)
 }
--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -11,6 +11,26 @@ suspend fun loadContributorsChannels(
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
     coroutineScope {
-        TODO()
+        val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .bodyList()
+
+        val channel = Channel<List<User>>()
+        repos.map {repo ->
+            launch {
+                val repoUsers = service.getRepoContributors(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+                channel.send(repoUsers)
+            }
+        }
+
+        var allUsers = emptyList<User>()
+        repeat(repos.size) {
+            val users = channel.receive()
+            allUsers = allUsers + users
+            updateResults(allUsers.aggregate(), it == repos.lastIndex)
+        }
     }
 }
